446c0b7d68ae8736397dab3f1ff2b64adeeec73d
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-04-11 13:52:52 -0700

    Changed page name meta data

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,8 +2,10 @@
   <v-container  text-xs-center fluid ma-0 pa-0 >
     <v-layout row wrap ma-0 pa-0 >
       <v-flex xs12 class="back-image">
-        <div class="title font-weight-light">Secure Your Daily Funds</div>
-        <div class="title font-weight-light">A Utility App That </div>
+        <div class="center">
+        <div class="display-3">Secure Your Daily Funds</div>
+        <div class="display-1">A utility app that helps you manage your daily use keys so that you don't loose them. </div>
+      </div>
       </v-flex>
       </v-layout>
   </v-container>
@@ -22,8 +24,8 @@ export default {
   background:
     /* top, transparent red, faked with gradient */
     linear-gradient(
-      rgba(211, 211, 211, 0.7),
-      rgba(211, 211, 211, 0.7)
+      rgba(13, 152, 186, 0.7),
+      rgba(13, 152, 186, 0.7)
     ),
     no-repeat center center fixed
     url(https://res.cloudinary.com/dylevfpbl/image/upload/v1555014467/decentralkeystorage/phone.jpg);
@@ -32,6 +34,14 @@ export default {
      -o-background-size: cover;
      background-size: cover;
      height:100vh;width:100vw;
+}
+
+.center {
+  margin: 0;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
 }
 
 </style>
